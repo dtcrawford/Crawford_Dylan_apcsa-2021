c92dcce98b2ea453061f963a28f48043cc428183
dtcrawford <33032943+dtcrawford@users.noreply.github.com>
2021-01-28 16:04:42 -0800

    Unit 01 final push

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,49 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   System.out.println("**********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   System.out.println("----------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println("\n\n");
+   }
+   
+   public void printASmallBox()
+   {	
+	   System.out.println("-----");
+	   for (int i = 0; i<5 ; i++ )
+		   System.out.println("-   -");
+	   System.out.println("-----");
+   }
+ 
+   public void printABigBox()
+   { 
+	   System.out.println("----------------------");;
+	   for (int i = 0; i<20 ; i++ )
+		   System.out.println("-                    -");
+	   System.out.println("----------------------");
+   }   
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,41 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   StarsAndStripes call = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+	   call.printTwentyDashes();
+	   call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwoBlankLines();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+	      call.printTwentyStars();
+	      call.printTwentyDashes();
+      
+   }
+}
