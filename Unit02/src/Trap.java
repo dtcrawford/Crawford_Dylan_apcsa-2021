fc9c9c4a8f13c685565af162038bbfeadabf36d4
dtcrawford <33032943+dtcrawford@users.noreply.github.com>
2021-02-01 15:34:18 -0800

    Unit 2 final push

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( double base1, double base2, double height )
+	{
+		return (((base1 + base2)/2) * height);
+	}
+}
